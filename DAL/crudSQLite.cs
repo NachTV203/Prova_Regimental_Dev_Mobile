2f5116bef8d8d4f4f773188423aa7091a1cb4937
Victor Alves <167913263+NachTV203@users.noreply.github.com>
2026-06-07 16:42:20 -0300

    Add files via upload

---
+++ DAL/crudSQLite.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using appProvaA1Caneta.Model;
+
+namespace appProvaA1Caneta.DAL
+{
+    public class crudSQLite
+    {
+        readonly SQLiteAsyncConnection _conexao;
+
+        public crudSQLite(string dbPath)
+        {
+            _conexao = new SQLiteAsyncConnection(dbPath);
+            _conexao.CreateTableAsync<Caneta>().Wait();
+        }
+
+        public Task<int> InsertCaneta(Caneta caneta)
+        {
+            return _conexao.InsertAsync(caneta);
+        }
+
+        public Task<List<Caneta>> GetCanetas()
+        {
+            return _conexao.Table<Caneta>().ToListAsync();
+        }
+
+        public Task<int> UpdateCaneta(Caneta caneta)
+        {
+            return _conexao.UpdateAsync(caneta);
+        }
+
+        public Task<int> DeleteCaneta(int id)
+        {
+            return _conexao.Table<Caneta>().DeleteAsync(c => c.canID == id);
+        }
+    }
+}
---
+++ Model/Caneta.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace appProvaA1Caneta.Model
+{
+    [Table("Caneta")]
+    public class Caneta
+    {
+        [PrimaryKey, AutoIncrement]
+        public int canID { get; set; }
+
+        public string canFabricante { get; set; }
+
+        public string canCor { get; set; }
+    }
+}
