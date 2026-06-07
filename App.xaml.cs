9af6cb62206e2458775af02b053183814908f292
Victor Alves <167913263+NachTV203@users.noreply.github.com>
2026-06-07 16:40:11 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using appProvaA1Caneta.DAL;
+using appProvaA1Caneta.Views;
+
+namespace appProvaA1Caneta;
+
+public partial class App : Application
+{
+    static crudSQLite database;
+
+    public static crudSQLite Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "bancocaneta.db3");
+                database = new crudSQLite(path);
+            }
+            return database;
+        }
+    }
+
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new NavigationPage(new TelaListaCaneta());
+    }
+}
